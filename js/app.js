3a070ae381c126c179045c18fa02013daf574489
Joel <joel.fagundo@gmail.com>
2017-10-18 10:44:26 -0500

    event click

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 const request = superagent
 let statesTable = document.querySelector('.users-list');
+const boton = document.querySelector('button')
+boton.addEventListener('click', function(){
 
 request
   .get('https://randomuser.me/api/?results=50')
   .then(function saveStatesInAVariable(data) {
-    console.log(data);
     const states = data.body.results
     return states
   })
   .then(function printInWebpage(statesList) {
+    statesTable.innerHTML = '';
     statesList.forEach(function(obj) {
       statesTable.innerHTML += `
                 <article>
@@ -23,3 +25,5 @@ request
     elem.innerHTML = 'Error al momento de obtener los datos'
     document.querySelector('.errors').appendChild(elem)
   })
+
+})
